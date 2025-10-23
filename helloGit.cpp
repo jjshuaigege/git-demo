a02b0519258f6e8a35462e6ecaab387c8c80bf26
shenguozhi <101167291+jjshuaigege@users.noreply.github.com>
2025-10-24 01:56:54 +0800

    写了第一个代码

---
+++ helloGit.cpp
@@ -0,0 +1,15 @@
+//
+// Created by SGZ on 2025-10-24.
+//
+
+
+#include <bits/stdc++.h>
+using namespace std;
+//设置控制台相关的头文件
+// # include "windows.h"
+//	SetConsoleOutputCP(65001); // 设置控制台输出编码为UTF-8 ,以解决中文乱码，注意：这两行代码仅在调试的时候用！！！
+int main(int argc, char* args[])
+{
+	cout << "hello git!" << endl;
+	return 0;
+}
